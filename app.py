b72bf87152f56922ec79c674d0a96bcb18b2961e
“faheemraza21” <faheemraza263@gmail.com>
2024-07-01 23:41:15 +0530

    9th commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-COHERE_API_KEY=st.secrets('COHERE_key')
+COHERE_API_KEY=st.secrets['COHERE_key']
 #COHERE_API_KEY= os.getenv('cohere_api')
 
 
